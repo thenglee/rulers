652445e56494a9aa5b11fd17a8fb343835a6cf3e
Tan Theng Lee <thengleetan@gmail.com>
2019-08-25 15:15:55 +0800

    Add Array library

---
+++ lib/rulers.rb
@@ -0,0 +1,13 @@
+require "rulers/array"
+require "rulers/version"
+
+module Rulers
+  class Error < StandardError; end
+
+  class Application
+    def call(env)
+      `echo debug > debug.txt`
+      [200, {'Content-Type' => 'text/html'}, ["Hello from Ruby on Rulers!"]]
+    end
+  end
+end
